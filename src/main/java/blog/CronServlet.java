f3d2427ce836bc014aaa707421721635f1ab5202
ajamafalous <32203238+ajamafalous@users.noreply.github.com>
2018-02-28 14:07:49 -0600

    Add files via upload
    aj upload 2/28 2:07

--- src/main/java/blog/CronServlet.java
+++ src/main/java/blog/CronServlet.java
@@ -1,40 +1,61 @@
 package blog;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.logging.Logger;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+@SuppressWarnings("serial")
 
 public class CronServlet extends HttpServlet {
-	
-	private static final Logger _logger = Logger.getLogger(CronServlet.class.getName());
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+public void doGet(HttpServletRequest req, HttpServletResponse resp)
+throws IOException {
+	String strCallResult = "";
+	resp.setContentType("text/plain");
+	try {
+		//Extract out the To, Subject and Body of the Email to be sent
+		String strTo = req.getParameter("email_to");
+		String strSubject = req.getParameter("email_subject");
+		String strBody = req.getParameter("email_body");
+		
+		//Do validations here. Only basic ones i.e. cannot be null/empty
+		//Currently only checking the To Email field
+		if (strTo == null) throw new Exception("To field cannot be empty.");
+		
+		//Trim the stuff
+		strTo = strTo.trim();
+		if (strTo.length() == 0) throw new Exception("To field cannot be empty.");
+		
+		//Call the GAEJ Email Service
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+
+		Message msg = new MimeMessage(session);
+		msg.setFrom(new InternetAddress("#YOUR EMAIL ADDRESS HERE#"));
+		msg.addRecipient(Message.RecipientType.TO,
+		
+		new InternetAddress(strTo));
+
+		msg.setSubject(strSubject);
+		msg.setText(strBody);
+		Transport.send(msg);
+
+		strCallResult = "Success: " + "Email has been delivered.";
+		resp.getWriter().println(strCallResult);
 	}
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-			try {
-			_logger.info("Cron Job has been executed");
-			//Put your logic here
-			//BEGIN
-			//END
-			}
-			catch (Exception ex) {
-			//Log any exceptions in your Cron Job
-			}
+	catch (Exception ex) {
+		strCallResult = "Fail: " + ex.getMessage();
+		resp.getWriter().println(strCallResult);
+	}
+}
+
+@Override
+public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException {
+		doGet(req, resp);
 	}
 }
